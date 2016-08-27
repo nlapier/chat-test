85e4b300685de6cb6200cb5f8c77e53498129fed
Nathaniel LaPier <Nat@Nathaniels-MacBook-Air.local>
2016-08-27 16:50:11 -0400

    adding id features

--- chatServer.js
+++ chatServer.js
@@ -3,10 +3,30 @@ var express = require ("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+// var moment = require('moment-timezone');
+// moment().tz("America/New_York").format();
 
 var PORT = process.env.PORT || 4000;
 
+var user = "nat";
+
 app.use(express.static('public'));
+
+//socket.io connect/disconnect listener
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('message', function(msg){
+ 	var sentMsg = user + ": " + msg
+    io.emit("message", sentMsg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
 //Route to chat box
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/chatIndex.html');
@@ -19,20 +39,15 @@ app.use(express.static('public'));
 //   });
 // });
 
-//socket.io connect/disconnect listener
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('message', function(msg){
-    console.log('message: ' + msg);
-    io.emit("message", msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-});
+// function sqlChat(chatText, chatUser){	
+//     var chatTime; // = moment.js timestamp
+//     var chatUser; // = user name
+//     var chatObj = {
+//       text: chatText,
+//       time: chatTime,
+//       user: chatUser
+//     }
+// }
 
 
 
